e8745bf42723cae5354817968ff4ac724f369bb6
Sam Avery <sam.avery1@gmail.com>
2022-10-31 16:58:42 -0500

    commit tree 5f9744f7430cdfe5f6d851678054fd9e3dd14002

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    students = Student.find(params[:id])
+    render json: students
+  end
+
+  def find
+    students = Student.find(params[:first_name])
+    render json: students
+  end
+
 end
